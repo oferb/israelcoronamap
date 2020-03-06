49daac7065a39dca166608c31a9c63d394c68e62
Ofer Bartal <blueofer@gmail.com>
2020-03-06 22:34:22 +0200

    More fixes

--- public/js/app.js
+++ public/js/app.js
@@ -1,35 +1,16 @@
 var map, infoWindow, govData;
 const windowWidth = window.screen.availWidth;
-let globalGovData = []
 let markersArray = [];
 
 function init() {
-  fetch('/json/data.json')
-    .then((response) => {
-      return response.json();
-    })
-    .then((govData) => {
-      const userData = getUserData();
-      const mockData = [
-        {
-          lat: 32.072810,
-          lon: 34.828020,
-          timestamp: Date.now()
-        }
-      ]
-      globalGovData = govData;
-      const exposurePosition = checkExposure(userData, mockData);
-      if (exposurePosition) {
-        alert("You have been near a person with Coronavirus: " + JSON.stringify(exposurePosition));
-      }
-    });
   getButtonElements();
   getGovData();
 }
 
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
-    center: { lat: 32.072958, lng: 34.969073 },
+    center: { lat: 32.071795, lng: 34.868469 },
+    
     zoom: windowWidth >= 500 ? 12 : 9,
     gestureHandling: "greedy"
   });
@@ -54,54 +35,6 @@ function navigate(lat, lng) {
   var center = new google.maps.LatLng(lat, lng);
   map.panTo(center);
 }
-
-function saveUserData(data) {
-  localStorage.setItem('user_data', JSON.stringify(data));
-}
-
-function getUserData() {
-  return JSON.parse(localStorage.getItem('user_data'));
-}
-
-function savePosition() {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function (position) {
-      var pos = {
-        lat: position.coords.latitude,
-        lng: position.coords.longitude,
-        timestamp: Date.now()
-      };
-
-      let userData = getUserData();
-      if (!userData) {
-        userData = [];
-      }
-      userData.push(pos);
-      saveUserData(userData);
-
-    }, function () {
-      alert("Position error");
-    });
-  } else {
-    // Browser doesn't support Geolocation
-    alert("Position error");
-  }
-}
-
-
-function checkExposure(userData, govData) {
-  for (let i = 0; i < userData.length; i++) {
-    for (let j = 0; j < govData.length; j++) {
-      if (userData[i].timestamp >= getTimestamp(govData[j].start_time) && userData[i].timestamp <= getTimestamp(govData[j].end_time)) {
-        if (getDistance(userData[i], govData[j]) <= govData[j].radius) {
-          return govData[j];
-        }
-      }
-    }
-  }
-  return null;
-}
-
 
 function getDistance(p1, p2) {
   return Math.sqrt(Math.pow(p2.lat - p1.lat, 2) + Math.pow(p2.lon - p1.lon, 2));
@@ -145,12 +78,11 @@ function updateMap() {
         scaledSize: new google.maps.Size(20, 20)
       }
     });
-    var contentStringCal = '<div class="infowindow">' + govData[j].description + '</div>';
-    console.log(contentStringCal);
 
     google.maps.event.addListener(marker, 'click', (function(marker, i) {
       return function() {
-        infoWindow.setContent(contentStringCal);
+        var contentString = '<div class="infowindow">' + govData[j].description + '</div>';
+        infoWindow.setContent(contentString);
         infoWindow.open(map, marker);
       }
     })(marker, j));
