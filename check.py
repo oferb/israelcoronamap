35b50c33ec3dc9adb8653ad380f10ec4f73d7a4b
Ofer Bartal <blueofer@gmail.com>
2020-03-11 18:40:52 +0200

    Formatting

--- check.py
+++ check.py
@@ -1,39 +1,19 @@
-from html.parser import HTMLParser
-from html.entities import name2codepoint
+from bs4 import BeautifulSoup
+import os
 
-class MyHTMLParser(HTMLParser):
-    def handle_starttag(self, tag, attrs):
-        print("Start tag:", tag)
-        for attr in attrs:
-            print("     attr:", attr)
+def getHtmlFiles():
+  result = []
+  for root, dirs, files in os.walk("public"):
+    for f in files:
+        if f.endswith(".html"):
+          result.append(os.path.join(root, f))
+  return result
 
-    def handle_endtag(self, tag):
-        print("End tag  :", tag)
+for filepath in getHtmlFiles():
+  with open(filepath, "r") as f:
+    htmlContents = f.read()
+  soup = BeautifulSoup(htmlContents, 'html.parser')
+  with open(filepath, "w") as f:
+    f.write(soup.prettify())
 
-    def handle_data(self, data):
-        print("Data     :", data)
-
-    def handle_comment(self, data):
-        print("Comment  :", data)
-
-    def handle_entityref(self, name):
-        c = chr(name2codepoint[name])
-        print("Named ent:", c)
-
-    def handle_charref(self, name):
-        if name.startswith('x'):
-            c = chr(int(name[1:], 16))
-        else:
-            c = chr(int(name))
-        print("Num ent  :", c)
-
-    def handle_decl(self, data):
-        print("Decl     :", data)
-
-parser = MyHTMLParser()
-
-
-htmlContents = ''
-with open('public/index.html', newline='') as f:
-  htmlContents = f.read()
-parser.feed(htmlContents)
+  
