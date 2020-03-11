db685f2b18736b68a541dddb520033f8e73581be
Ofer Bartal <blueofer@gmail.com>
2020-03-11 16:31:08 +0200

    Design changes

--- check.py
+++ check.py
@@ -1,15 +1,39 @@
 from html.parser import HTMLParser
+from html.entities import name2codepoint
 
 class MyHTMLParser(HTMLParser):
     def handle_starttag(self, tag, attrs):
-        print("Encountered a start tag:", tag)
+        print("Start tag:", tag)
+        for attr in attrs:
+            print("     attr:", attr)
 
     def handle_endtag(self, tag):
-        print("Encountered an end tag :", tag)
+        print("End tag  :", tag)
 
     def handle_data(self, data):
-        print("Encountered some data  :", data)
+        print("Data     :", data)
+
+    def handle_comment(self, data):
+        print("Comment  :", data)
+
+    def handle_entityref(self, name):
+        c = chr(name2codepoint[name])
+        print("Named ent:", c)
+
+    def handle_charref(self, name):
+        if name.startswith('x'):
+            c = chr(int(name[1:], 16))
+        else:
+            c = chr(int(name))
+        print("Num ent  :", c)
+
+    def handle_decl(self, data):
+        print("Decl     :", data)
 
 parser = MyHTMLParser()
-parser.feed('<html><head><title>Test</title></head>'
-            '<body><h1>Parse me!</h1></body></html>')
+
+
+htmlContents = ''
+with open('public/index.html', newline='') as f:
+  htmlContents = f.read()
+parser.feed(htmlContents)
--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,5 +1,62 @@
 
 
+$(document).ready(function () {
+    
+  // don't hide pop up when click on this items
+  $(".hamburger").click(function (e) {
+    e.stopPropagation();
+  });
+
+  // on body click anywhere - hide side bar pop-up
+  $("body").click(function () {
+    $('.side-bar').fadeOut('fast');
+    $('#overlay').removeClass('overlay');
+            $( '.hamburger' ).removeClass( "is-active" );
+
+  });
+
+  // side-bar menu open
+  $('.hamburger').click(function () {
+    $('.side-bar').fadeToggle(50);
+    $( this ).toggleClass( "is-active" );
+    $('#overlay').toggleClass('overlay');
+  });
+
+  // terms-close-x icon
+  $('#terms-close-x').click(function () {
+    $('#terms-of-use-pop-up').fadeOut('fast');
+  });
+
+
+    // open terms of use pop-up
+  $('.terms-of-use-pop-up-click').click(function () {
+    $('#terms-of-use-pop-up').fadeIn("fast");
+  });
+
+
+  // open embed pop-up
+  $('.code-embed-pop-up-click').click(function () {
+
+    $('#embedCoronaMap').modal('show');
+
+  });
+  
+  // open map reader pop-up
+  $('.map-reader-pop-up-click').click(function () {
+
+    $('#map-reader-pop-up').fadeIn("fast");
+
+  });
+  
+// map-reader-close-x icon
+  $('#map-reader-close-x').click(function () {
+    $('#map-reader-pop-up').fadeOut('fast');
+  });
+
+
+
+});
+
 /*! track-focus v 1.0.0 | Author: Jeremy Fields [jeremy.fields@vget.com], 2015 | License: MIT */
 // inspired by: http://irama.org/pkg/keyboard-focus-0.3/jquery.keyboard-focus.js
 
