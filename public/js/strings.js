fb5242b0d163c5e6bcf9ceb98b9013cd0ba7793b
Ofer Bartal <blueofer@gmail.com>
2020-03-12 21:13:53 +0200

    Data update

--- public/js/strings.js
+++ public/js/strings.js
@@ -1,6 +1,6 @@
 
 function init() {
-    document.getElementById("last-updated-time").textContent="12.3.2020, 19:15";
+    document.getElementById("last-updated-time").textContent="12.3.2020, 21:15";
 }
 
 init();
