331322eddb6bffe2e8e275a77fd71459cea25ac6
Ofer Bartal <blueofer@gmail.com>
2020-03-18 02:08:13 +0200

    Data update

--- public/js/strings.js
+++ public/js/strings.js
@@ -2,7 +2,7 @@
 const isOnEmbed = window.location.pathname.includes('embed');
 
 const initUpdatedTime = () => {
-  const updatedTime = "17.3.2020, 14:50";
+  const updatedTime = "18.3.2020, 02:00";
   if (isOnEmbed) {
     document.getElementById("last-updated-time-embed").textContent = updatedTime;
   } else {
@@ -11,8 +11,8 @@ const initUpdatedTime = () => {
 };
 
 const sickDataUpdate = () => {
-  const updatedTime = "17.3.2020, 14:50";
-  const numberOfSickPeople = 324;
+  const updatedTime = "18.3.2020, 02:00";
+  const numberOfSickPeople = 337;
   const numberOfRecovered = 11;
   const numberOfDeaths = 0;
   const numberOfPeopleInQuarantine = '50,337';
