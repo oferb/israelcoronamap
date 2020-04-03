74805a2babe43e2d609c27ed0059ae0ea94556c0
Ofer Bartal <blueofer@gmail.com>
2020-04-03 18:34:12 +0300

    Add feedback to embed

--- public/js/feedback.js
+++ public/js/feedback.js
@@ -50,8 +50,6 @@ const showFeedbackPopup = () => {
   });
   $('#report-pop-up').fadeIn('fast');
   setQueryParam('feedback', 'true');
-  // clean previous success message
-  showSuccessMessage(false);
 };
 
 $('#submit-feedback-button').click(async () => {
@@ -64,7 +62,6 @@ $('#submit-feedback-button').click(async () => {
   });
 
   cleanErrors();
-  showSuccessMessage(false);
   showLoaderAndPreventDoubleClick();
   const selectedCity = $('#select-city-input :selected').text();
   const selectedFeeling = $('.feeling-box.selected');
@@ -77,102 +74,7 @@ $('#submit-feedback-button').click(async () => {
   if (!selectedFeeling.length) {
     feedbackError.push('הרגשה');
   }
-
-  if (feedbackError.length) {
-    // Missing fields
-    $('.error-filling-feedback').css('display', 'block');
-    $('#missing-feedback-fields').text(feedbackError.join(', '));
-    showSuccessMessage(false);
-    resetButton();
-  } else {
-
-    if (!isUserSigned) {
-      await firebase.auth().signInAnonymously().catch((error) => {
-        console.error(error);
-      });
-    }
-
-    const userId = firebase.auth().currentUser.uid;
-    const cityId = $('#select-city-input').val();
-    const county = getCounty(cityId);
-    const feeling = $('.feeling-box.selected .feeling-text').text();
-    await saveFormToDB({
-      id: userId,
-      city: selectedCity,
-      cityId,
-      county,
-      feeling
-    });
-  }
 });
-
-const showLoaderAndPreventDoubleClick = () => {
-  $('#feedback-button-spinner').css('display', 'block');
-  $('#feedback-button-text').css('display', 'none');
-  $('#submit-feedback-button').addClass('disable-double-click');
-};
-
-const resetButton = () => {
-  $('#feedback-button-spinner').css('display', 'none');
-  $('#feedback-button-text').css('display', 'block');
-  $('#submit-feedback-button').removeClass('disable-double-click');
-};
-
-const cleanErrors = () => {
-  $('.error-filling-feedback').css('display', 'none');
-  $('#missing-feedback-fields').text('');
-  $('.network-error-filling-feedback').css('display', 'none');
-};
-
-const showSuccessMessage = (show) => {
-  $('.form-success-message').css('display', show ? 'block' : 'none');
-  $('#submit-feedback-button').css('display', show ? 'none' : 'inline-block');
-};
-
-const saveFormToDB = async ({
-  id,
-  city,
-  cityId,
-  county,
-  feeling
-}) => {
-
-  db.collection("feedback").add({
-    id,
-    city,
-    county,
-    feeling,
-    date: new Date().getTime()
-  })
-    .then(() => {
-      setTimeout(() => {
-        resetButton();
-        showSuccessMessage(true);
-      }, 1000);
-      localStorage.setItem("feedback-city-id", cityId);
-    })
-    .catch((error) => {
-      console.error(error);
-
-      setTimeout(() => {
-        resetButton();
-        $('.network-error-filling-feedback').css('display', 'block');
-        showSuccessMessage(false);
-      }, 1000);
-
-      ga('send', {
-        hitType: 'event',
-        eventCategory: 'FeedbackFailedOnFirestore',
-        eventAction: 'Write',
-        eventLabel: 'Failed to write feedback to DB'
-      });
-    });
-};
-
-const getCounty = (cityId) => {
-  const cityIdAsInt = parseInt(cityId);
-  return CITIES.find((city) => city.id === cityIdAsInt).county;
-};
 
 if (getQueryParam('feedback') === 'true') {
   showFeedbackPopup();
