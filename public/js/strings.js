765f36f61922318351b507f180bc5a8e12124ba9
Ofer Bartal <blueofer@gmail.com>
2020-04-04 21:48:56 +0300

    Data update

--- public/js/strings.js
+++ public/js/strings.js
@@ -6,10 +6,10 @@ const sickElementUpdate = (id, value) => {
 };
 
 const sickDataUpdate = () => {
-  const updatedTime = "4.4.2020 11:45";
-  const numberOfSickPeople = 7589;
-  const numberOfRecovered = 427;
-  const numberOfDeaths = 42;
+  const updatedTime = "4.4.2020 21:50";
+  const numberOfSickPeople = 7851;
+  const numberOfRecovered = 458;
+  const numberOfDeaths = 43;
 
   sickElementUpdate("number-of-sick-people-text", numberOfSickPeople);
   sickElementUpdate("number-of-recovered-people-text", numberOfRecovered);
