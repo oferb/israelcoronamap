4547879145b78b06047292333ea17a7fb828dbea
Mosh Feu <moshfeu@gmail.com>
2020-04-05 19:35:13 +0300

    Save my location in localStorage (#120)

--- public/js/utils.js
+++ public/js/utils.js
@@ -1,9 +1,10 @@
+// @ts-check
 const getQueryParam = (name) => {
   const queryString = window.location.search;
   const urlParams = new URLSearchParams(queryString);
   return urlParams.get(name);
 };
-  
+
 const setQueryParam = (name, value) => {
   let urlParams = new URLSearchParams(window.location.search);
   urlParams.set(name, value);
@@ -17,3 +18,27 @@ const deleteQueryParam = (name) => {
   urlParams.delete(name);
   window.history.pushState('Corona map', 'Corona map', window.location.pathname + '?' + urlParams.toString());
 };
+
+/**
+ * @typedef {{
+ *  zoom: number,
+ *  center: {
+ *    lat: number,
+ *    lng: number
+ *  }
+ * }} MapBounds
+ */
+
+/**
+ * @param {MapBounds} mapLocation
+ */
+const setBoundsToLocalStorage = (mapLocation) => {
+  localStorage.setItem('mapLocation', JSON.stringify(mapLocation));
+}
+
+/**
+ * @returns {MapBounds}
+ */
+const getBoundsFromLocalStorage = () => {
+  return JSON.parse(localStorage.getItem('mapLocation') || 'null');
+}
